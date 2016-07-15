dc906c45b5e78c166d1fc097e5da099d125f2721
Jim Oxenford <joxenford@me.com>
2016-07-15 06:01:54 -0400

    add API

--- app/src/main/java/com/thinkupllc/weatherforecast/DarkSkyAPI.java
+++ app/src/main/java/com/thinkupllc/weatherforecast/DarkSkyAPI.java
@@ -2,25 +2,30 @@ package com.thinkupllc.weatherforecast;
 
 import retrofit2.Call;
 import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 import retrofit2.http.GET;
 
 public interface DarkSkyAPI {
 
-    String BASE_URL = "https://api.forecast.io/forecast";
+    String BASE_URL = "https://api.forecast.io/forecast/";
 
-    @GET("/cbaa9628d88936e14d77ecdba8aefa2e/39.942990,-75.26986") Call<Weather> getWeather();
-
-
+    @GET("cbaa9628d88936e14d77ecdba8aefa2e/39.942990,-75.26986") Call<Weather> getWeather();
 
     class Factory {
 
-        private static DarkSkyAPI se
+        private static DarkSkyAPI service;
+        public static DarkSkyAPI getInstance() {
+            if (service == null) {
+                Retrofit retrofit = new Retrofit.Builder().addConverterFactory(GsonConverterFactory.create())
+                        .baseUrl(BASE_URL)
+                        .build();
 
-        Retrofit retrofit = new Retrofit.Builder()
-                .baseUrl(BASE_URL)
-                .build();
-
-        DarkSkyAPI service = retrofit.create(DarkSkyAPI.class);
+                service = retrofit.create(DarkSkyAPI.class);
+                return service;
+            } else {
+                return service;
+            }
+        }
     }
 
 }
